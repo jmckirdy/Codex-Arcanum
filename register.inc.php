eefd2b195a53b8f138b7594ecc6a873057d3457e
John McKirdy <57973241+jmckirdy@users.noreply.github.com>
2024-04-06 15:39:31 -0700

    moved flexbox css to be used by register

--- register.inc.php
+++ register.inc.php
@@ -1,33 +1,38 @@
-<h3>Welcome to the Codex Arcanum!</h3>
+<div class='flex-container'>
+    <!-- <div class="flex-item"> -->
 
-<p>A repository for your programming notes that when reviewed, updates the note, and tells you what and when you should review next.</p>
+    <h3>Welcome to the Codex Arcanum!</h3>
 
-<h3>Register to upload books, codices, spells, and cast enchantments.</h3>
-<br>
-<form action="index.php" method="post">
+    <p>A repository for your programming notes that when reviewed, updates the note, and tells you what and when you should review next.</p>
 
-<b>User Name:</b><br><input type="text" name="userid"><br>
+    <h3>Register to upload books, codices, spells, and cast enchantments.</h3>
+    <br>
+    <form action="index.php" method="post">
 
-<br />
+    <b>User Name:</b><br><input type="text" name="userid"><br>
 
-<b>Password:</b><br><input type="password" name="password"><br>
+    <br />
 
-<br />
+    <b>Password:</b><br><input type="password" name="password"><br>
 
-<font size="1">Confirm Password:</font><br /> 
+    <br />
 
-<input type="password" name="password2"><br>
+    <font size="1">Confirm Password:</font><br /> 
 
-<br />
+    <input type="password" name="password2"><br>
 
-<b>E-mail address:</b><br><input type="text" size="50" name="email"><br>
+    <br />
 
-<font size="2">Privacy Policy: we do not share your information with others</font><br><br>
+    <b>E-mail address:</b><br><input type="text" size="50" name="email"><br>
 
-<input type="submit" value="Submit">
+    <font size="2">Privacy Policy: we do not share your information with others</font><br><br>
 
-<input type="hidden" name="content" value="adduser">
+    <input type="submit" value="Submit">
 
-<hr size="1" noshade="noshade">
+    <input type="hidden" name="content" value="adduser">
 
-</form>
+    <hr size="1" noshade="noshade">
+
+    </form>
+    <!-- </div> -->
+</div>
