25cba52162479e64568a22c0b518e3dbb5ea8060
John McKirdy <57973241+jmckirdy@users.noreply.github.com>
2024-04-12 16:30:02 -0700

    css updates news.inc.php

--- news.inc.php
+++ news.inc.php
@@ -5,15 +5,28 @@
     }
 </style>
 
-<h3>Tavern Gossip <i class="fa-solid fa-hat-wizard"></i></h3>
-
-<br>Inspiration: Codex's Jewel bound codex pic
-<hr>
-
-<br>
 <h3>Hit-counter</h3>
 <br>This site has <span id="hits">12354</span> Hits!
 <br><br><hr>
+
+
+<script src="library/startMusic.js"></script>
+<audio id="startMusic" src="assets/Hero_Quest_Piano.mp3"></audio> 
+<!-- ⬇️  ⬇️ -->
+<h3>⬇️ Music Sample ⬇️</h3>
+<!-- <br> -->
+<p style="font:normal 32px Times, serif; text-align:center;">▶️</p>
+<hr>
+
+
+<br>What is a Codex? This is:
+<img alt="codex" src="assets/Codex_Aureus_Sankt_Emmeram.jpg" width="250px">
+<p>This is the Codex Aureus Sankt Emmeram, a 9th-century illuminated Gospel Book which takes its name from Saint Emmeram's Abbey, where it was for most of its history. <br><br>
+It is lavishly illuminated, (text decorated with flourishes) and it's cover is ensconced with gems and relief figures in gold. <br><br>
+Precisely dated to 870, it is an important example of Carolingian art, as well as one of very few surviving treasure bindings of this date.</p>
+<hr>
+<br>
+
 
 <h3>Weekly Show</h3>
 <?php
@@ -21,15 +34,9 @@ $variable = 'Mythic Quest';
 echo "<br>$variable";
 ?><br>
 <a href="https://www.imdb.com/title/tt8879940/">imdb link</a>
-
-
-
 <hr>
-<script src="library/startMusic.js"></script>
-<audio id="startMusic" src="assets/Hero_Quest_Piano.mp3"></audio> 
 
-<!-- ⬇️  ⬇️ -->
-<h3>Weeking Music</h3>
-<br>
-<font size="14" onclick="playMusic()">▶️</font>
+
+<h3>Tavern Gossip <i class="fa-solid fa-hat-wizard"></i></h3>
+<p>Have you heard? That lusty Argonian maid from Riften hooked up with the DragonBorn.<br><br> I can scarecly believe that's his type!</p>
 <hr>
