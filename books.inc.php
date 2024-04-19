bbab5028774d7b65dc3c5d7706aadcde7b640cf2
John McKirdy <57973241+jmckirdy@users.noreply.github.com>
2024-04-18 23:23:56 -0700

    indentations, tested diff files and sorted into 'bad_data' folder

--- books.inc.php
+++ books.inc.php
@@ -3,7 +3,7 @@ include('library/login.php');
 $con = login();
 
 // $id = 28; // Assuming you're fetching a specific PDF by its ID
-$id = 8; 
+$id = 4; 
 
 // $query = "SELECT file_data, file_name FROM pdf_files WHERE id = $id";
 // $result = mysqli_query($con, $query) or die('Sorry, could not retrieve PDF');
@@ -24,7 +24,7 @@ $id = 8;
 // }
 
 
-
+// SELECTING PDF WINDOW
 $query = "SELECT id, file_name, file_data FROM pdf_files WHERE id = $id";
 $result = mysqli_query($con, $query) or die('Sorry, could not find the requested PDF');
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC) or die('No records retrieved');
@@ -37,46 +37,43 @@ echo "<div class='flex-container'>";
     // echo "by $poster <br><br>\n";
     echo "<div class='flex-item'>";
 
+        // Output the file name
+        echo "<h2>$file_name</h2>";
 
-    // Output the file name
-    echo "<h2>$file_name</h2>";
-
-    // Output an iframe to embed the PDF file,    --had this  id="flex-item-books" 
-    // echo '<style class="flex-container-damn"> width:100%;background-color=red; height=700%</style>';
-    // echo '<iframe class="flex-container" src="data:application/pdf;base64,' . base64_encode($file_data) . '" width="100%" height="700%"></iframe>';
-    echo '<iframe class="flex-container" src="data:application/pdf;base64,' . base64_encode($file_data) . '"></iframe>';
-
-    
+        // Output an iframe to embed the PDF file
+        echo '<iframe class="flex-container" src="data:application/pdf;base64,' . base64_encode($file_data) . '"></iframe>';
     echo "</div>";
-echo "</div>";
+    //  CONTINUATION OF MAIN FLEX (previous <div>s with echo"" and no indentation are pretty unreadable)
 ?>
 
 
 
 <?php
-echo "<div class='flex-container'>";
-    echo "<h2>$file_name</h2>\n";
-    // echo "by $poster <br><br>\n";
-
-    echo "<h3>Content</h3><br>\n";
+    /////////// NEW FLEX ITEM CSS
     echo "<div class='flex-item'>";
-        $query = "SELECT comment from comments where codexid = $id order by commentid desc";
-        $result = mysqli_query($con, $query) or die('Could not retrieve enchantments');
+        echo "<h2>$file_name</h2>\n";
+        // echo "by $poster <br><br>\n";
 
-        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-        {
-            // $date = $row['date'];
-            // $poster = $row['poster'];
-            $comment = $row['comment'];
-            $comment = nl2br($comment);
-            echo "-enchantment cast!<br>\n";
-            echo "$comment\n";
+        echo "<h3>Content</h3><br>\n";
+        
+            // SELECTING COMMENTS
+            $query = "SELECT comment from comments where codexid = $id order by commentid desc";
+            $result = mysqli_query($con, $query) or die('Could not retrieve enchantments');
+
+            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+            {
+                // $date = $row['date'];
+                // $poster = $row['poster'];
+                $comment = $row['comment'];
+                $comment = nl2br($comment);
+                echo "-enchantment cast!<br>\n";
+                echo "$comment\n";
+                echo "<br><br>\n";
+            }
             echo "<br><br>\n";
-        }
-        echo "<br><br>\n";
 
+        echo "</div>";
     echo "</div>";
-echo "</div>";
 ?>
 
 <!-- <div>
