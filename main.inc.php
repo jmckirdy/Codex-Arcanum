88f25b45b4a5af703a6b9d4984838b1ed6722c7e
John McKirdy <57973241+jmckirdy@users.noreply.github.com>
2024-04-20 20:17:53 -0700

    damn css, the categories are laid out in rows now. pay attention to the inline css of the img on line 32:main.inc

--- main.inc.php
+++ main.inc.php
@@ -2,7 +2,7 @@
    <h2>Codex Arcanum <i class="fa-solid fa-hat-wizard"></i></h2>
 
    <p>The Latest Books, Codices, and Spells<br><br>
-   <div class="flex-item">
+   <div id="flex-item">
 
 <?php
 include("library/login.php");
@@ -17,7 +17,7 @@ if (mysqli_num_rows($result) == 0)
 } 
 else
 {
-   echo "<table class=\"galaxy-table\">";
+   echo "<div class=\"galaxy-cats\">";
    while($row=mysqli_fetch_array($result, MYSQLI_ASSOC))
    {
 
@@ -25,17 +25,25 @@ else
       $title = $row['title'];
       $picture = $row['picture'];
 
-      // //TODO::---- will need to change the url when getting content of category.
-      // // look at register/validate any file with uploading the catid num
- 
-      echo "<tr>";
-      echo "<td class=\"left-column\"><a href=\"index.php?content=showcategories&id=$catid\"><img src=\"showimage.php?catid=$catid\" width=\"100%\" height=\"190px\"></a></td>";
-      echo "<td class=\"tiny-cell\"><a href=\"index.php?content=showcategories&id=$catid\">&nbsp;</a></td>";
-      echo "<td class=\"right-column\"><a href=\"index.php?content=showcategories&id=$catid\">$title</a></td>";
-      echo "</tr>";     
+      //TODO::---- will need to change the url when getting content of category.
+      // look at register/validate any file with uploading the catid num
+      echo "    <div class=\"far-left\">";
+      echo "        <a href=\"index.php?content=showcategories&id=$catid\">";
+      echo "            <img src=\"showimage.php?catid=$catid\" width=\"auto\" height=\"150px\" alt=\"Image\">";  // IMPORTANT SIZE CSS
+      echo "        </a>";
+      echo "    </div>";
+      echo "    <div class=\"spacer\">";
+      echo "        <a href=\"index.php?content=showcategories&id=$catid\">";
+      echo "    </div>";
+      echo "    <div class=\"far-right\">";
+      echo "        <a href=\"index.php?content=showcategories&id=$catid\">$title</a>";
+      echo "    </div>";
+      echo "<br>\n";
+
    }
-   echo "</table>\n";
+   echo "</div>\n";
 }
+echo "<br>";
 ?>
 
 
